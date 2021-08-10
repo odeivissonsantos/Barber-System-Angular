dc107359f5aec4d712b4aef6a66225c45e7c1efe
odeivissonsantos <deivissonsantos@hotmail.com>
2021-08-10 19:51:10 -0300

    finalizando metodo salvar

--- src/app/views/cliente/cliente-criar/cliente-criar.component.ts
+++ src/app/views/cliente/cliente-criar/cliente-criar.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { ClienteService } from './../cliente.service';
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from '../cliente.model';
@@ -15,14 +16,18 @@ export class ClienteCriarComponent implements OnInit {
     telefone: '',
   }
 
-  constructor(private service: ClienteService) { }
+  constructor(private service: ClienteService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
   salvar(): void {
     this.service.salvar(this.cliente).subscribe((resposta) => {
-      
+      this.router.navigate(['clientes'])
     })
+  }
+
+  voltar(): void {
+    this.router.navigate(['clientes'])
   }
 }
