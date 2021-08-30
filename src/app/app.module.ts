e4af8112729fd6a5de782f94c01a0580ef44339c
odeivissonsantos <deivissonsantos@hotmail.com>
2021-08-30 19:03:28 -0300

    melhoria nos components e inclusão de mais uma aba - agendamento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { FooterComponent } from './footer/footer.component';
-import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './views/home/home.component';
 import { ClienteListarComponent } from './views/cliente/cliente-listar/cliente-listar.component';
 import { ClienteCriarComponent } from './views/cliente/cliente-criar/cliente-criar.component';
@@ -28,7 +27,6 @@ import { ServicoprestadoAtualizarComponent } from './views/servicoprestado/servi
     AppComponent,
     ToolbarComponent,
     FooterComponent,
-    NavComponent,
     HomeComponent,
     ClienteListarComponent,
     ClienteCriarComponent,
